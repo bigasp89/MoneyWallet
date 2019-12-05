3d70d33ad460652414e98d451256f9020365254d
Giacomo Bigazzi <giacomo.bigazzi20@gmail.com>
2019-12-05 18:28:50 +0100

    add barchar

--- app/src/main/java/trexmoney/it/moneyWallet/MainActivity.java
+++ app/src/main/java/trexmoney/it/moneyWallet/MainActivity.java
@@ -1,44 +1,30 @@
 package trexmoney.it.moneyWallet;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import android.view.View;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
-
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.Menu;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-
+    private BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -51,13 +37,15 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        setUpNavigation();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void setUpNavigation(){
+        bottomNavigation =findViewById(R.id.bottom_nav_bar);
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigation,
+                navHostFragment.getNavController());
     }
 
     @Override
--- app/src/main/java/trexmoney/it/moneyWallet/ui/home/HomeFragment.java
+++ app/src/main/java/trexmoney/it/moneyWallet/ui/home/HomeFragment.java
@@ -14,10 +14,15 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
@@ -29,6 +34,7 @@ public class HomeFragment extends Fragment {
     private float[] yData = {80f,20f};
     private String[] xData = {"Uscite","Entrate"};
     PieChart pieChart;
+    BarChart barChart;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -49,11 +55,42 @@ public class HomeFragment extends Fragment {
         pieChart.setHoleRadius(70f);
         pieChart.setCenterTextSize(16);
         pieChart.setCenterTextTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
-        pieChart.setCenterText("-600 €");
+        pieChart.setCenterText("-60 €");
         pieChart.setTransparentCircleAlpha(0);
         pieChart.setDrawEntryLabels(false);
         pieChart.getLegend().setEnabled(false);
+        pieChart.animateXY(1000, 1000);
         addDataSetPie();
+
+        barChart = root.findViewById(R.id.barchart);
+
+        ArrayList NoOfEmp = new ArrayList();
+
+        NoOfEmp.add(new BarEntry(945f, 0));
+        NoOfEmp.add(new BarEntry(1040f, 1));
+        NoOfEmp.add(new BarEntry(1133f, 2));
+        NoOfEmp.add(new BarEntry(1240f, 3));
+        NoOfEmp.add(new BarEntry(1369f, 4));
+        NoOfEmp.add(new BarEntry(1487f, 5));
+
+
+        ArrayList year = new ArrayList();
+
+        year.add("2008");
+        year.add("2009");
+        year.add("2010");
+        year.add("2011");
+        year.add("2012");
+        year.add("2013");
+
+
+        BarDataSet bardataset = new BarDataSet(NoOfEmp, "No Of Employee");
+        barChart.animateY(5000);
+        BarData data = new BarData(bardataset);
+        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
+        barChart.setData(data);
+        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
+
         return root;
     }
     private void addDataSetPie(){
